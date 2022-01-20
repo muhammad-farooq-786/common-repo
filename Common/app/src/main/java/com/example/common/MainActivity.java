4c37ce4f641dfa29607f564959c2c57a9df8da4d
Eskool18 <eskooleims@gmail.com>
2022-01-20 21:50:38 +0500

    added List and recycler view getters

--- Common/app/src/main/java/com/example/common/MainActivity.java
+++ Common/app/src/main/java/com/example/common/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.common;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    List<Friends> friendsList = new ArrayList<Friends>();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
